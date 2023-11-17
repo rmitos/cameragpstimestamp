8e9c32620a98b33c37a725562bef5d19b1d68545
rmitos <mobileapps@rmitsolutions.net>
2023-11-17 15:24:44 +0530

    Removed provider.

--- cameragpstimestamp/src/main/java/net/rmitsolutions/camera_gps_timestamp/CameraGpsPhotoFileProvider.kt
+++
@@ -1,8 +0,0 @@
-package net.rmitsolutions.camera_gps_timestamp
-
-import androidx.core.content.FileProvider
-
-class CameraGpsPhotoFileProvider : FileProvider() {
-    //extend fileprovider to stop collision with other library file provider
-    //this class is empty and used in manifest
-}
